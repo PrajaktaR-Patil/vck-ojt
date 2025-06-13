f1be7be8ec64e4aaf37d3bb3778e134b1366d783
PrajaktaR-Patil <prajktapatil2005@gmail.com>
2025-06-13 12:50:44 +0530

    fourth commit

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ import ChatbotComponent from './components/Chatbot/ChatbotComponents';
 import { useState } from 'react';
 
 import DeveloperInfoPopup from './components/DeveloperInfo/DeveloperInfoPopup';
+import Headers from './components/Header/Headers';
 
 
 
@@ -47,6 +48,7 @@ function App() {
      <main style={{ padding: '1rem', backgroundColor:"#52d2d2"}}>
 
           <Routes>
+            <Headers/>
             <Route path="/" element={<HomePage />} />
             <Route path="/about" element={<AboutPage />} />
             <Route path="/apply" element={<AdmissionsPage />} />
